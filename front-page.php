b5b0aaf08c5aad043cce8378090d53a1278b69e2
Craig Fowler <craig.fowler@gmail.com>
2018-10-05 09:14:06 -0700

    fixed home

--- front-page.php
+++ front-page.php
@@ -5,17 +5,19 @@
 
 <hr class="invisible">
 
+<?php $image = get_field('home_ad_image',336); ?>
+<?php if($image): ?>
 <div class="fs-row">
   <div class="fs-cell fs-lg-8 fs-md-full fs-sm-3 fs-all-justify-center">
-    <?php $image = get_field('home_ad_image',336); ?>
-    <a href="<?php the_field('home_ad_link',336); ?>"><img src="<?php echo $image['url']; ?>" class="img-responsive"></a>
+    <a href="<?php the_field('home_ad_link',336); ?>"><img src="<?php echo $image; ?>" class="img-responsive"></a>
   </div>
 </div>
+<?php endif; ?>
 
 <hr class="invisible">
 
 <?php get_template_part( 'parts/home', 'latest' ); ?>
-<?php get_template_part( 'parts/home', 'ad' ); ?>
+<?php #get_template_part( 'parts/home', 'ad' ); ?>
 <?php #get_template_part( 'parts/home', 'stories' ); ?>
 </div>
 
