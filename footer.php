94f64beaeea726b8db5df65bf00be7e7b529c924
Craig Fowler <craig@howler.la>
2018-10-10 10:44:48 -0700

    added mobile unit

--- footer.php
+++ footer.php
@@ -19,6 +19,16 @@
     </footer>
   </div><!--Wrapper-->
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-81120400-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-81120400-1');
+</script>  
+
 <script src="<?php print VENDOR_SCRIPTS_DIR . 'vendor.min.js'; ?>"></script>
 <script src="<?php print SCRIPTS_DIR . 'main.min.js'; ?>"></script>
 <?php wp_footer(); ?>
--- front-page.php
+++ front-page.php
@@ -6,11 +6,17 @@
 <hr class="invisible">
 
 <?php $image = get_field('home_ad_image',336); ?>
+<?php $imageMobile = get_field('home_ad_image_mobile',336); ?>
 <?php if($image): ?>
 <div class="fs-row">
-  <div class="fs-cell fs-lg-8 fs-md-full fs-sm-3 fs-all-justify-center">
+  <div class="fs-cell fs-lg-8 fs-md-full fs-sm-hide fs-all-justify-center">
     <a href="<?php the_field('home_ad_link',336); ?>"><img src="<?php echo $image; ?>" class="img-responsive"></a>
   </div>
+  <?php if($imageMobile): ?>
+  <div class="fs-cell fs-lg-hide fs-md-hide fs-sm-3 fs-all-justify-center">
+    <a href="<?php the_field('home_ad_link',336); ?>"><img src="<?php echo $imageMobile; ?>" class="img-responsive"></a>
+  </div>
+  <?php endif; ?>
 </div>
 <?php endif; ?>
 
